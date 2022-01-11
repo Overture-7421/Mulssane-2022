2342059d405797fd07fb8282d21ce88f096ea190
Jorge-PG <jorgepg@tec.mx>
2022-01-11 17:43:52 -0600

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit bc3666422976a7e8639d49368b73eb5edb6e6cab.

--- src/Autonomous/TurnToAngle/TurnToAngle.cpp
+++ src/Autonomous/TurnToAngle/TurnToAngle.cpp
@@ -14,7 +14,7 @@ TurnToAngle::TurnToAngle(Chassis* chassis, double angleObjective) {
 // Called when the command is initially scheduled.
 void TurnToAngle::Initialize() {
 
-  turnToAnglePID.EnableContinuousInput(-180_deg, 180_deg);
+  turnToAnglePID.EnableContinuousInput(-180, 180);
   //frc::SmartDashboard::PutNumber("P", 0);
   //frc::SmartDashboard::PutNumber("I", 0);
   //frc::SmartDashboard::PutNumber("D", 0);
@@ -23,9 +23,10 @@ void TurnToAngle::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void TurnToAngle::Execute() {
+  turnToAnglePID.SetSetpoint(angleObjective);
   currentAngle = chassis->getPose().Rotation().Degrees().value();
 
-  double angularVelocity = turnToAnglePID.Calculate(units::degree_t(currentAngle), units::degree_t(angleObjective));
+  double angularVelocity = turnToAnglePID.Calculate(currentAngle);
 
   frc::ChassisSpeeds vels;
   vels.omega = units::radians_per_second_t(angularVelocity);
