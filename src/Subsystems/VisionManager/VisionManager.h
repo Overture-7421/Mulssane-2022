905b8b84cad401e550783ba57e50979804a0705b
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2022-03-03 16:00:25 -0600

    Added constants for camera, after they moved it to the great stick

--- src/Subsystems/VisionManager/VisionManager.h
+++ src/Subsystems/VisionManager/VisionManager.h
@@ -59,10 +59,10 @@ class VisionManager : public frc2::SubsystemBase {
 };
 
 namespace CameraConstants {
-  const frc::Rotation2d pitch {30_deg};
-  const units::meter_t height = 0.67_m;
+  const frc::Rotation2d pitch {35_deg};
+  const units::meter_t height = 37.2_in;
   const double vpw = 2.0 * units::math::tan(58.5_deg / 2.0);
   const double vph = 2.0 *  units::math::tan(45.6_deg / 2.0);
   const std::pair<double, double> resolution {640, 480};
-  const frc::Transform2d cameraToRobot {{0_m, 0_m}, 0_deg};
+  const frc::Transform2d cameraToRobot {{-13_in, 10.5_in}, 0_deg};
 }
