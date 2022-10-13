4bb4f48f680458aac91eef6d9fe90e41f6a47597
Santiago Quintana <59663686+SantiQ0905@users.noreply.github.com>
2022-10-13 15:20:49 -0500

    Shooter with vision updates

--- src/Commands/Common/SetShooterWithVision/SetShooterWithVision.h
+++ src/Commands/Common/SetShooterWithVision/SetShooterWithVision.h
@@ -37,8 +37,8 @@ class SetShooterWithVision
   Hood* hood;
   VisionManager* visionManager;
   LinearInterpolator distanceVsVelocityInterpolator {{
-    {1.5, 220},
-    {2.25, 220},
+    {1.5, 225},
+    {2.25, 227},
     {3.0, 250},
     {3.75, 277},
     {4.5, 290}
